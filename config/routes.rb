6940586d1a8ca02eef77252630936ab572b4cbc4
kaydee0502 <kshitijdhama513@gmail.com>
2021-05-13 20:05:57 +0530

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :users
+  #get 'sessions/new'
+  #get 'sessions/create'
+  #get 'sessions/login'
+  #get 'sessions/welcome'
+  #get 'users/new'
+  #get 'users/create'
   #get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
